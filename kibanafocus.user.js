88a419247c23fa056fec789d452ea87ff89d08f6
Issung <joel@sonofgoran.com>
2023-03-30 22:54:11 +1100

    Added fav column saving + restoring
    Added the ability to set your favourite columns and then restore them, also restores them on discover page open.
    Refactored code that decodes url to be reusable nicely.
    Fixed a bug when using the 'focus on traceid' button when you have some text in the query search box.
    Added a random message of the day.

--- kibanafocus.user.js
+++ kibanafocus.user.js
@@ -1,32 +1,161 @@
 // ==UserScript==
 // @name         Kibana TraceId Focus
-// @version      0.1
+// @version      0.2
 // @description  Add a "focus" link to traceidentifiers that wipes all filters except for timeframe and adds a filter for the traceidentifier.
 // @author       JoelG AKA Issung
 // @match        https://search-elkelasticsearchdomain-bqedehxv6l7akoeyshisnm72g4.us-west-2.es.amazonaws.com/_plugin/kibana/app/*
 // @match        https://search-ekelasticsearchdomain-5jvrnkc5zt3m5nevaxazbnnmp4.eu-central-1.es.amazonaws.com/_plugin/kibana/app/*
 // @icon         https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt601c406b0b5af740/620577381692951393fdf8d6/elastic-logo-cluster.svg
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @require      https://cdn.jsdelivr.net/npm/rison@0.1.1/js/rison.js
 // ==/UserScript==
 
 (function() {
     'use strict';
+    const FAVCOLS_KEY = 'KIBANAFOCUS_FAVCOLS';
+    let motds = [
+        'good luck!',
+        'you\'re welcome!',
+        'hello world!',
+        'abandon hope all ye who enter here...',
+        'remember to thank Joel!',
+        'boy I\'m useful!',
+        `the time is ${new Date().toLocaleTimeString()}.`,
+        `the date is ${new Date().toDateString()}.`,
+        'have a great day!',
+        'bababooey!',
+        'nevermind...',
+        'I hope these messages brighten your day!',
+        'I got one job and i\'m good at it!',
+        'let\'s dance!',
+        'beers after this?',
+        'the log has to be in here somewhere..',
+        'stop reading these messages!',
+        'close that Twitter tab! I see it!',
+        'that\'s all.',
+        'stay safe!',
+        'wanna hear a joke?',
+        'sue me!',
+        'I\'m running out of messages.',
+        'happy birthday!',
+        'remember to drink some water.',
+    ];
+
+    console.log('%c KibanaFocus userscript running! Written by JoelG/Issung', 'font-size: 36px; font-weight: bold');
+
+    function getUrlParts() {
+        var hash = window.location.hash;
+        var questionMarkIndex = hash.indexOf('?'); // TODO: Trim # and / up until the question mark instead.
+        var queryParamsString = hash.substr(questionMarkIndex);
+        const params = new Proxy(new URLSearchParams(queryParamsString), {
+            get: (searchParams, prop) => searchParams.get(prop),
+        });
+
+        var g = rison.decode(params._g);
+        var a = rison.decode(params._a);
+
+        return [g, a];
+    }
+
+    function saveFavouriteColumns() {
+        let [g, a] = getUrlParts();
+        var columnsJoined = a.columns.join(',');
+        GM.setValue(FAVCOLS_KEY, columnsJoined);
+        console.log(`${FAVCOLS_KEY} set to ${columnsJoined}`);
+        var columnsMessageElement = document.querySelector('#favourite-columns-message');
+        if (columnsMessageElement != null) {
+            columnsMessageElement.innerHTML = `Favourites saved as [${columnsJoined}]`;
+        }
+    }
+
+    // Set custom columns if none set.
+    async function restoreFavouriteColumns() {
+        let [g, a] = getUrlParts();
+        let defaults = `'@service',level,message`;
+        var favColumns = (await GM.getValue(FAVCOLS_KEY, defaults)).split(',');
+        console.log(`${FAVCOLS_KEY} loaded ${favColumns} (defaults are ${defaults})`);
+        a.columns = favColumns;
+        var gEncoded = rison.encode(g);
+        var aEncoded = rison.encode(a);
+        var url = `https://${window.location.host}/_plugin/kibana/app/discover#/?_g=${gEncoded}&_a=${aEncoded}`;
+        window.location = url;
+        var columnsMessageElement = document.querySelector('#favourite-columns-message');
+        if (columnsMessageElement != null) {
+            columnsMessageElement.innerHTML = `Columns restored to favourites [${favColumns}]`;
+        }
+    }
+
+    function addColumnButtons() {
+        // First check if already added
+        if (document.querySelector('div.modification#favourite-columns') != null) {
+            return;
+        }
+
+        // The elements we insert the column buttons inbetween load after page load
+        // So we check every 100ms for the elements existence, and when they appear we insert, simple!
+        var timeChartElement = document.querySelector('section.dscTimechart');
+        if (timeChartElement == null) {
+            setTimeout(addColumnButtons, 100);
+        }
+        else {
+            let [g, a] = getUrlParts();
+            var motd = motds[Math.floor(Math.random()*motds.length)];
+            timeChartElement.insertAdjacentHTML('afterend', `
+                <div class="modification" id="favourite-columns">
+                    <button action="save" alt="Set your favourite columns to the current setup">Save favourite columns</button>
+                    <button action="restore" alt="Load your favourite columns and use them in the UI">Restore favourite columns</button>
+                    <p id="favourite-columns-message">Columns automatically restored to [${a.columns}], ${motd}</p>
+                    <a style="float: right;" href="https://github.com/Issung/KibanaFocus">
+                        <img class="issung" src="https://i.imgur.com/Zfb2K30.png"/>
+                    </a>
+                    <span class="tooltip" style="float: right;">
+                        <a href="https://github.com/Issung/KibanaFocus">Kibana focus plugin by JoelG</a>
+                    </span>
+                </div>
+            `);
+            // We can't put onclick events on the buttons because of CSP, so this is the solution..
+            document.querySelectorAll('div#favourite-columns button').forEach(btn => {
+                btn.addEventListener('click', event => {
+                    if (event.target.attributes.action.value == 'save') {
+                        saveFavouriteColumns();
+                    }
+                    else if (event.target.attributes.action.value == 'restore') {
+                        restoreFavouriteColumns();
+                    }
+                });
+            });
+        }
+    }
 
     // Custom styling
     document.querySelector('head').insertAdjacentHTML('beforeend', `
         <style>
+            .modification#favourite-columns {
+                display: block;
+                padding: 5px;
+            }
+            .modification#favourite-columns #favourite-columns-message {
+                display: inline;
+                font-family: 'Roboto Mono';
+                font-size: 11px;
+            }
+            .modification#favourite-columns button {
+                text-decoration: underline;
+                color: black;
+                margin-right: 10px;
+                font-size: 14px;
+            }
             div.modification {
                 display: inline;
             }
             div.modification a:first-child:hover {
                 text-decoration: underline;
             }
-            .modification a img.issung {
+            .modification img.issung {
                 height:16px;
                 width:16px;
                 position: relative;
-                top: 4px;
             }
             .modification a + span.tooltip {
                 display: none;
@@ -47,8 +176,39 @@
         </style>
     `);
 
+    // Setup column favourites
+    /*document.querySelector('section.dscTimechart').after(`
+        <div class="modification" id="favourite-columns">
+            <button>Save favourite columns</button>
+            <button>Restore favourite columns</button>
+        </div>
+    `);*/
+
+    addColumnButtons();
+
+    // On page load, if no columns are set then set them now.
+    if (window.location.hash.indexOf('columns:!(_source)') != -1) {
+        restoreFavouriteColumns();
+    }
+
+    // Add listener for if the discovery page is opened, then restore favourite columns.
+    window.addEventListener("hashchange", (event) => {
+        if (event.oldURL == '' && event.oldURL == '') {
+            return;
+        }
+        // Did we navigate from elsewhere to the discover page?
+        if (event.oldURL.endsWith('#/') && event.newURL.indexOf('#/?_g=(') != -1) {
+            console.log(`Doing some setup again because hashchanged to what we think is the discover page. oldURL: ${event.oldURL}, newURL: ${event.newURL}`);
+            addColumnButtons();
+            if (event.newURL.indexOf('columns:!(_source)') != -1) {
+                restoreFavouriteColumns();
+            }
+        }
+    });
+
+    // Setup on log-expand traceid focus button generation
     document.addEventListener("click", function(e) {
-        console.log(`click event on ${e.target} ${e.target.outerHTML}`);
+        //console.log(`click event on ${e.target} ${e.target.outerHTML}`);
 
         var isExpandButton = e.target.attributes?.['aria-label']?.value == 'Toggle row details' ?? false;
         var isRightArrowSvg = e.target.querySelector('path')?.attributes?.d?.value.startsWith('M5.157') ?? false;
@@ -72,7 +232,7 @@
                         <div class="modification">
                             <a style="margin-left: 20px;" href="${url}">🔍 Focus on traceidentifier</a>
                             <a style="margin-left: 20px;" href="https://github.com/Issung/KibanaFocus">
-                                <img class="issung" src="https://i.imgur.com/Zfb2K30.png"/>
+                                <img style="top: 4px;" class="issung" src="https://i.imgur.com/Zfb2K30.png"/>
                             </a>
                             <span class="tooltip">
                                 <a href="https://github.com/Issung/KibanaFocus">Kibana focus plugin by JoelG</a>
@@ -87,15 +247,7 @@
             });
 
             function generateUrl(fieldName, fieldValue) {
-                var hash = window.location.hash;
-                var questionMarkIndex = hash.indexOf('?'); // TODO: Trim # and / up until the question mark instead.
-                var queryParamsString = hash.substr(questionMarkIndex);
-                const params = new Proxy(new URLSearchParams(queryParamsString), {
-                    get: (searchParams, prop) => searchParams.get(prop),
-                });
-
-                var g = rison.decode(params._g);
-                var a = rison.decode(params._a);
+                let [g, a] = getUrlParts();
 
                 a.filters = [
                     {
@@ -120,6 +272,8 @@
                         },
                     },
                 ];
+
+                a.query.query = ""; // Retain preferred query type (lucene or kql), but remove the searchboxtext
 
                 var gEncoded = rison.encode(g);
                 var aEncoded = rison.encode(a);
